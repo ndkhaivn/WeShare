d96baba4e0b5a71551ad502c450921119aa35793
ndkhaivn <ndkhaivn@gmail.com>
2020-05-21 23:23:10 +0700

    Initial Commit

---
+++ WeShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeShare
+//
+//  Created by Nguyễn Đình Khải on 21/5/20.
+//  Copyright © 2020 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
